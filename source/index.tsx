1a470c4f982044a9d3b82eaa78ecbf9c2c3fe6f6
LuisGazcon <luiselgazcon@gmail.com>
2021-11-07 22:24:51 -0800

    feat: chakra initialized in project

--- source/index.tsx
+++ source/index.tsx
@@ -2,12 +2,15 @@ import './polyfill';
 import React, { StrictMode } from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import { application, store } from '@/application';
 
 render(
 	<StrictMode>
-		<Provider store={store}>{application}</Provider>
+		<ChakraProvider>
+			<Provider store={store}>{application}</Provider>
+		</ChakraProvider>
 	</StrictMode>,
 	document.querySelector('#root'),
 );
