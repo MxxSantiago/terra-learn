fb3a03f837ad5a4415a5aef9b57cc6afbc9f6605
LuisGazcon <luiselgazcon@gmail.com>
2021-11-12 01:37:34 -0800

    feat(core): actions are now binded with the redux dispatch function

--- core/common/index.ts
+++ core/common/index.ts
@@ -1,11 +1,27 @@
 import { moduleContext } from '@core/context/module';
+import { useDispatch } from 'react-redux';
+import { Dispatch } from 'redux';
 
 export function getActions<T = any>(moduleToken: string): T {
 	const { actions } = moduleContext.get(moduleToken);
 	return actions;
 }
 
-export function getSelector<T = any>(moduleToken: string): T {
+export function getSelectors<T = any>(moduleToken: string): T {
 	const { selectors } = moduleContext.get(moduleToken);
 	return selectors;
+}
+
+export function useActions<T = any>(moduleToken: string): T {
+	const dispatch = useDispatch();
+	const actions = getActions<T & Actions>(moduleToken);
+	actions.setDispatch(dispatch);
+	return actions;
+}
+
+export class Actions {
+	public dispatch: Dispatch;
+	public setDispatch = (dispatch: Dispatch) => {
+		this.dispatch = dispatch;
+	};
 }
