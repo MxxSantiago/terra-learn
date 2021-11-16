e6285c54ca21e0704dbc917dd343b3ef5c06f9a2
LuisGazcon <luiselgazcon@gmail.com>
2021-11-15 18:16:27 -0800

    feat: removed unused import from application.ts

--- source/application.ts
+++ source/application.ts
@@ -3,7 +3,6 @@ import { applicationFactory } from '@core/common/application';
 import App from '@/app';
 import { mainModule } from '@/modules/main/main.module';
 import { authModule } from '@/modules/auth/auth.module';
-import { moduleContext } from '@core/context/module';
 
 export const { application, store } = applicationFactory({
 	component: App,
